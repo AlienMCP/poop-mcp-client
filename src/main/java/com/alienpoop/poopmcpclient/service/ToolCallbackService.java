7cb66a5f9b372b11ce6b11fca6c080bb1806cf25
fangyeming <fangyemingzz@gmail.com>
2025-06-22 11:48:26 +0800

    refactor ToolCallbackService to add scheduled pinging of ToolCallbackProvider and enhance logging

--- src/main/java/com/alienpoop/poopmcpclient/service/ToolCallbackService.java
+++ src/main/java/com/alienpoop/poopmcpclient/service/ToolCallbackService.java
@@ -1,10 +1,13 @@
 package com.alienpoop.poopmcpclient.service;
 
+import jakarta.annotation.PostConstruct;
 import java.util.Arrays;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.model.function.FunctionCallback;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,6 +15,7 @@ import org.springframework.stereotype.Component;
  * invoking getToolCallbacks(). If an error occurs, it triggers an application shutdown.
  */
 @Component
+@Slf4j
 public class ToolCallbackService {
 
   @Autowired private ToolCallbackProvider toolCallbackProvider;
@@ -23,5 +27,22 @@ public class ToolCallbackService {
       functionCallbackList = Arrays.asList(toolCallbackProvider.getToolCallbacks());
     }
     return functionCallbackList;
+  }
+
+  @Scheduled(cron = "0 * * * * ?")
+  @PostConstruct
+  public void ping() {
+
+    try {
+
+      log.info("Pinging ToolCallbackProvider...");
+
+
+      toolCallbackProvider.getToolCallbacks();
+    } catch (Exception e) {
+
+      log.error("Error while pinging ToolCallbackProvider: ", e);
+      System.exit(500);
+    }
   }
 }
